b87605ae9dc02d5342d1e7c439bb350e181268f2
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2021-02-15 09:47:48 +0700

    Changed repo structure (again)

--- Data Structures/Linked List/Cython/README.md
+++
@@ -1,2 +0,0 @@
-# Description
-Check Cython's documentation about how to compile cython's code: https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#compilation
--- Data Structures/Linked List/Cython/Wrapping C code/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-
-ext = Extension('CyLinkedList', sources = ['CyLinkedList.pyx', 'SinglyLinkedList.c'])
-
-setup(ext_modules = cythonize([ext]))
